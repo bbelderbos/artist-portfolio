328dd9300620008ba40feae9a7f64018f8819178
Bob Belderbos <bobbelderbos@gmail.com>
2024-06-13 17:54:10 +0200

    with thumbs, lightbox, navbar

--- src/handlers.rs
+++ src/handlers.rs
@@ -7,18 +7,20 @@ use axum::{
 use crate::Config;
 use crate::s3::get_images;
 use std::sync::Arc;
+use std::collections::HashMap;
 
 #[derive(Template)]
 #[template(path = "about.html")]
 struct AboutTemplate {
     image_url: String,
+    current_page: &'static str,
 }
 
 pub async fn about_handler(Extension(config): Extension<Arc<Config>>) -> Result<Html<String>, StatusCode> {
     let image_key = "artist.png";
     let image_url = format!("https://{}.s3.amazonaws.com/{}", config.aws_s3_bucket, image_key);
 
-    let template = AboutTemplate { image_url };
+    let template = AboutTemplate { image_url, current_page: "about" };
     match template.render() {
         Ok(rendered) => Ok(Html(rendered)),
         Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
@@ -28,12 +30,15 @@ pub async fn about_handler(Extension(config): Extension<Arc<Config>>) -> Result<
 #[derive(Template)]
 #[template(path = "portfolio.html")]
 struct PortfolioTemplate {
-    image_urls: Vec<String>,
+    images: Vec<(String, String)>,
+    current_page: &'static str,
 }
 
 pub async fn portfolio_handler(Extension(config): Extension<Arc<Config>>) -> Result<Html<String>, StatusCode> {
-    let images = get_images(&config.aws_s3_bucket).unwrap_or_else(|_| vec![]);
-    let template = PortfolioTemplate { image_urls: images };
+    let images = get_images(&config.aws_s3_bucket).unwrap_or_else(|_| HashMap::new());
+    // cannot get the template to work with a HashMap directly, so convert to a Vec of tuples
+    let images: Vec<(String, String)> = images.into_iter().collect();
+    let template = PortfolioTemplate { images, current_page: "portfolio"  };
 
     match template.render() {
         Ok(rendered) => Ok(Html(rendered)),
--- src/s3.rs
+++ src/s3.rs
@@ -1,10 +1,14 @@
-pub fn get_images(aws_s3_bucket: &str) -> Result<Vec<String>, String> {
-    let mut image_urls = Vec::new();
-    // hardcoded for now to keep it simple, but in a real-world scenario
+use std::collections::HashMap;
+use std::error::Error;
+
+pub fn get_images(aws_s3_bucket: &str) -> Result<HashMap<String, String>, Box<dyn Error>> {
+    let mut images = HashMap::new();
+    // Hardcoded for now to keep it simple, but in a real-world scenario
     // you would fetch the image URLs from an S3 bucket
     for i in 1..=10 {
-        let image_url = format!("https://{}.s3.amazonaws.com/{}.webp", aws_s3_bucket, i);
-        image_urls.push(image_url);
+        let full_image = format!("https://{}.s3.amazonaws.com/{}.webp", aws_s3_bucket, i);
+        let thumbnail = full_image.replace(".webp", "_thumb.png");
+        images.insert(full_image, thumbnail);
     }
-    Ok(image_urls)
+    Ok(images)
 }
